89eea4ddee1faf2054b2ec9dbf42ebc44ffc583c
Sergio Sanchez Zahinos <sersanch@student.42barcelona.com>
2023-02-22 17:45:35 +0100

    updated

--- src/map_checker.c
+++ src/map_checker.c
@@ -6,14 +6,57 @@
 /*   By: sersanch <sersanch@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 15:51:06 by sersanch          #+#    #+#             */
-/*   Updated: 2023/02/22 15:53:53 by sersanch         ###   ########.fr       */
+/*   Updated: 2023/02/22 17:45:29 by sersanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	check_map_characters(char **map)
 {
 	char	*line;
+	int		i;
 	
-	line = get_next_line(map);
+	while (1)
+	{
+		line = get_next_line(map);
+		if (!line)
+			break;
+		i = 0;
+		while (line[i])
+		{
+			if (ft_strchr(ALLOWED_CHARS, line[i]) == -1)
+				return (1);
+			i++;
+		}
+	}
+	return (0);
+}
 
+int	map_is_rectangular(char **map)
+{
+	
+}
+
+char **format_array_map(char **map)
+{
+	char	**formated_map;
+	char	**aux;
+	char	*line;
+	int		i;
+
+	formated_map = NULL;
+	while (1)
+	{
+		line = get_next_line(map);
+		if (!line)
+			break;
+		aux = formated_map;
+		i = 0;
+		while (line[i])
+		{
+			if (ft_strchr(ALLOWED_CHARS, line[i]) == -1)
+				return (1);
+			i++;
+		}
+	}
+	return (0);
 }
