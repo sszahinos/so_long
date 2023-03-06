7a9e444876ca489996f8c5e73423c1f9ef6aef71
Sergio Sanchez Zahinos <sersanch@student.42barcelona.com>
2023-03-06 15:35:10 +0100

    updated funct map checker

--- src/map_checker.c
+++ src/map_checker.c
@@ -6,12 +6,18 @@
 /*   By: sersanch <sersanch@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 15:51:06 by sersanch          #+#    #+#             */
-/*   Updated: 2023/02/22 17:45:29 by sersanch         ###   ########.fr       */
+/*   Updated: 2023/03/06 15:34:56 by sersanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+/* Input: 
+ * 	map text
+ * Output:
+ * 	0 -> map has correct characters
+ * 	1 -> map has forbidden characters
+ */
 int	check_map_characters(char **map)
 {
 	char	*line;
@@ -79,31 +85,44 @@ int map_get_rows(const char *map_path)
 	}
 }
 
+/* Checks if the path has a rectangular map.
+ * Input:
+ * 	map path
+ * Output:
+ * 	-1 -> failed to open map 
+ * 	1 -> map is rectangular
+ * 	0 -> map is not rectangular
+ */
 int map_is_rectangular(const char *map_path)
 {
 
+	int		correct_rows;
+	int		result;
 	int		rows;
 	int		cols;
 	char	*row;
 
+	correct_rows = 0;
+	result = -1;
 	cols = map_get_columns(map_path);
 	map_fd = open(map_path, O_RDONLY);
 	if (map_fd == -1)
-	{
-		close(map_fd);
 		return (-1);
-	}
 	while (1)
 	{
 		row = get_next_line(map_fd);
-		if (!row) // free row? controlar que tenga mínimo 5 rows (3x3 interior)?
-		{
-			close(map_fd);
-			return (1);
-		}
-		if (row[cols] != '\n' || row[cols] != '\0')
-			return (0);
+		if (!row && correct_rows >= 5)
+			result = 1;
+		else if ((!row && correct_rows < 5) || (row[cols] != '\n' && row[cols] != '\0'))
+			result = 0;
+		correct_rows++;
+		if (result != -1)
+			break;
 	}
+	if (row)
+		free(row);
+	close(map_fd);
+	return (result);
 }
 
 t_map *format_map(const char *map_path) //Se asume que se ha controlado que es correcto el mapa
